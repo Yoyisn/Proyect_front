ea8831d768e68feb82b264b51c90dad24d443e53
Jordy <105098782+Yoyisn@users.noreply.github.com>
2023-11-09 20:10:55 -0500

    Update axios.js

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'servitech-azure.vercel.app/api',
+    baseURL: 'servitech-azure.vercel.app',
     withCredentials: true,
 });
 
